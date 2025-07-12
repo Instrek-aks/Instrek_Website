a3a51b55408bbcfb8aa5cedd025dcbcbb9133500
Ajay Kumar Saraswat <ajaykumarsaraswat9045@gmail.com>
2025-07-12 16:09:00 +0530

    cards remove hover effct

--- frontend/src/components/ServiceDetail.jsx
+++ frontend/src/components/ServiceDetail.jsx
@@ -32,7 +32,7 @@ export default function DroneCard({
 
   return (
     <div
-      className=" border boder-white relative w-full h-[400px] md:h-[400px] overflow-hidden rounded-lg text-white cursor-pointer transform transition-transform duration-300 hover:scale-105"
+      className=" border boder-white relative w-full h-[400px] md:h-[400px] overflow-hidden rounded-lg text-white cursor-pointer"
       onClick={handleClick}
     >
       {/* Background Image */}
@@ -62,10 +62,10 @@ export default function DroneCard({
           <p className="text-sm md:text-xl leading-relaxed text-white">
             {description}
           </p>
-          <div className="flex items-center text-lg md:text-2xl font-semibold hover:text-[#ea4820] transition-colors duration-300">
+          <div className="flex items-center text-lg md:text-2xl font-semibold">
             Learn more
-            <span className="ml-2 w-6 h-6 md:w-8 md:h-8 bg-[#ea4820] rounded-full flex items-center justify-center group-hover:bg-white transition-colors duration-300">
-              <ArrowRight className="w-3 h-3 md:w-4 md:h-4 text-white group-hover:text-[#ea4820]" />
+            <span className="ml-2 w-6 h-6 md:w-8 md:h-8 bg-[#ea4820] rounded-full flex items-center justify-center">
+              <ArrowRight className="w-3 h-3 md:w-4 md:h-4 text-white" />
             </span>
           </div>
         </div>
